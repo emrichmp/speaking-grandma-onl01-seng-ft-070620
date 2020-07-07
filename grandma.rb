418bdfdbb12eac4903a0730c4fbcf28f774b2bbb
Emrich-Michael Perrier <emrichmp@gmail.com>
2020-07-07 21:02:37 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,10 +2,10 @@
 def speak_to_grandma(phrase)
   if phrase == phrase.upcase
     puts "NO, NOT SINCE 1938!"
-    elsif phrase == phrase.downcase
-      puts "HUH?! SPEAK UP, SONNY!"
-    elsif phrase == "I LOVE YOU GRANDMA!"
-      puts "I LOVE YOU TOO PUMPKIN!"
+  elsif phrase == phrase.downcase
+    puts "HUH?! SPEAK UP, SONNY!"
+  elsif phrase == "I LOVE YOU GRANDMA!"
+    puts "I LOVE YOU TOO PUMPKIN!"
   end
 end
 
